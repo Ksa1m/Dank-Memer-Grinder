5fd402789c0ddd724847d483f0109d50b4dc1347
Ksa1m <139200440+Ksa1m@users.noreply.github.com>
2025-02-02 20:50:40 +0100

    Update Config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-# Set to True if the stream feature is unlocked. To verify, use the command /stream in Dank Memer.
+# Set to True if the stream feature is unlocked. To verify, use the command /stream in Dank Memer
 stream_unlocked = False
 
-# Enables the option to keep the window on top of other windows.
+# Enables the option to keep the window on top of other windows
 window_enabled = True
 
-# Activates safe mode to avoid executing potentially harmful commands.
+# Activates safe mode to avoid executing potentially harmful commands
 safe_mode = True
 
-# If commands do not register properly when typed normally (e.g., "/hunt"), increase this value.
+# If commands do not register properly when typed normally (e.g., "/hunt"), increase this value
 load_time = 0.3
 
-# Defines the key used to toggle the bot's activation.
+# Defines the key used to toggle the bot's activation
 key = "F8"
